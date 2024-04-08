f28318ad47d551a35ebd77f2e83b14df423b07ab
Carlos França <carlosfranca.dev@gmail.com>
2024-04-08 16:24:21 -0300

    colocando dados adicionais
    Alterei algumas coisinhas no Views, e comecei a testar para puxar os dados do banco.
    
    Ja coloquei também todas as tabelas que, inicialmente, seão necessárias.
    
    Foi  configurado tbm para que a página possa ler e armazenar as imagens no banco.

--- loja/urls.py
+++ loja/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import homepage
+from .views import *
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('loja/', loja, name='loja'),
+    path('carrinho/', carrinho, name='carrinho'),
+    path('checkout/', checkout, name='checkout'),
+    path('minhaconta/', minha_conta, name='minha_conta'),
+    path('login/', login, name='login'),
 ]
